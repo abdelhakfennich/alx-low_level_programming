55ae023f11f0628732417af4d9b0e4c37cfbc8ec
abdelhakfennich <fennichabdelhak2@gmail.com>
2024-07-17 07:02:54 -0700

    mzn

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@ int main ()
 	int c = sizeof (long int);
 	int d = sizeof (long long int);
 	int e = sizeof (float);	
-	printf("Size of a char : %d byte(s)\n", a);
+	printf("Size of a char: %d byte(s)\n", a);
 	printf("Size of an int: %d byte(s)\n", b);
 	printf("Size of a long int: %d byte(s)\n", c);
 	printf("Size of a long long int: %d byte(s)\n", d);
